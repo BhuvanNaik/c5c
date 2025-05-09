ecc001bdc272ffc72ec1fa4389cec887b2bb8cd2
Bhuvan Manjunath Naik <151733205+BhuvanNaik@users.noreply.github.com>
2025-05-09 21:30:15 +0530

    Convert project to JavaScript

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,29 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  js.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    files: ['**/*.{js,jsx}'],
     plugins: {
+      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,5 +33,10 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
-);
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import ProblemStatement from './components/ProblemStatement';
+import TargetUsers from './components/TargetUsers';
+import Features from './components/Features';
+import TechStack from './components/TechStack';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-[#8F00FF] to-black">
+      <Navbar />
+      <Hero />
+      <ProblemStatement />
+      <TargetUsers />
+      <Features />
+      <TechStack />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
